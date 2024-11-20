6b36052a348511d7a504f569035209b116999497
Luca <61409445+melusc@users.noreply.github.com>
2024-11-20 10:02:29 +0100

    Add options

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,46 +1,51 @@
 import tsEslint from 'typescript-eslint';
 
-export default [
-	...tsEslint.configs.strictTypeChecked,
-	{
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				tsconfigRootDir: import.meta.dirname,
+/**
+ * @param {readonly string[]} globs 
+ */
+export default function typescript(globs) {
+	return [
+		...tsEslint.configs.strictTypeChecked,
+		{
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					tsconfigRootDir: import.meta.dirname,
+				},
+			},
+			rules: {
+				'@typescript-eslint/restrict-template-expressions': [
+					'error',
+					{
+						allowNumber: true,
+					},
+				],
+				'@typescript-eslint/unbound-method': 'off',
+				'@typescript-eslint/no-non-null-assertion': 'off',
+				'@typescript-eslint/no-unused-vars': [
+					'error',
+					{
+						vars: 'all',
+						varsIgnorePattern: /^_/.source,
+						args: 'after-used',
+						ignoreRestSiblings: true,
+						argsIgnorePattern: /^_/.source,
+						caughtErrors: 'all',
+						caughtErrorsIgnorePattern: /^_$/.source,
+					},
+				],
+				'@typescript-eslint/no-misused-promises': [
+					'error',
+					{
+						checksConditionals: true,
+						// Useful for event handlers
+						checksVoidReturn: false,
+					},
+				],
 			},
 		},
-		rules: {
-			'@typescript-eslint/restrict-template-expressions': [
-				'error',
-				{
-					allowNumber: true,
-				},
-			],
-			'@typescript-eslint/unbound-method': 'off',
-			'@typescript-eslint/no-non-null-assertion': 'off',
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					vars: 'all',
-					varsIgnorePattern: /^_/.source,
-					args: 'after-used',
-					ignoreRestSiblings: true,
-					argsIgnorePattern: /^_/.source,
-					caughtErrors: 'all',
-					caughtErrorsIgnorePattern: /^_$/.source,
-				},
-			],
-			'@typescript-eslint/no-misused-promises': [
-				'error',
-				{
-					checksConditionals: true,
-					// Useful for event handlers
-					checksVoidReturn: false,
-				},
-			],
-		},
-	},
-].map(config => ({
-	files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
-	...config,
-}));
+	].map(config => ({
+		...config,
+		files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts', ...globs],
+	}));
+}
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
 import type {Linter} from 'eslint';
 declare const config: readonly Linter.Config[];
 export default config;
+
+type Options = {
+	typescriptGlobs: readonly string[],
+}
+
+export function withOptions(options: Options): readonly Linter.Config[];
