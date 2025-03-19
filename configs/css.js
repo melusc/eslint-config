ebb844c3f308aef81ef292d5044722e216fc1231
Luca <oss@lusc.ch>
2025-03-20 05:24:45 +1000

    feat!: @eslint/css (#43)

---
+++ configs/css.js
@@ -0,0 +1,20 @@
+import css from '@eslint/css';
+
+const {rules, plugins} = css.configs.recommended;
+
+export default [
+	{
+		files: ['**/*.css'],
+		plugins,
+		language: 'css/css',
+		rules: {
+			...rules,
+			'css/require-baseline': [
+				'warn',
+				{
+					available: 'newly',
+				},
+			],
+		},
+	},
+];
